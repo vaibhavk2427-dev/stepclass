4bf927d21d96a8bab666f3c44d0256312e977ee8
vaibhavk2427-dev <vaibhavk2427@gmail.com>
2026-02-17 10:07:01 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello {
+    public static void main(String[] args) {
+        
+        int firstNumber = 10;
+        int secondNumber = 20;
+        
+        // Adding the numbers
+        int sum = firstNumber + secondNumber;
+        
+        // Displaying the result
+        System.out.println("The sum is: " + sum);
+    }
+}
